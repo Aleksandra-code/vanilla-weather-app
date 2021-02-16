605d212cf35ab712805e7d08b0e833be9e61b79e
Aleksandra <78027205+Aleksandra-code@users.noreply.github.com>
2021-02-16 18:24:04 +0100

    Added the search engine

--- src/index.js
+++ src/index.js
@@ -36,8 +36,19 @@ iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.dat
 iconElement.setAttribute("alt", response.data.weather[0].description); 
 }
 
+function search(city) {
 let apiKey = "db968106084657387e9f88fbdfeea89a";
-let city = "New York";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-
 axios.get(apiUrl).then(displayTemperature);
+}
+
+function handleSubmit(event) {
+    event.preventDefault();
+    let cityInputElement = document.querySelector("#city-input");
+    search(cityInputElement.value);
+}
+
+search("Valencia");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
