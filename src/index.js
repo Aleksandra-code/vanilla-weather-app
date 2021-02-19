735439e5e2fcd8b0a284ef406c87148b38a46098
Aleksandra <78027205+Aleksandra-code@users.noreply.github.com>
2021-02-19 10:30:40 +0100

    Implemented unit converter

--- src/index.js
+++ src/index.js
@@ -25,6 +25,7 @@ let windElement = document.querySelector("#wind");
 let dateElement = document.querySelector("#date");
 let iconElement = document.querySelector("#weather-icon");
 
+celciusTemperature = response.data.main.temp;
 
 temperatureElement.innerHTML = Math.round(response.data.main.temp);
 cityElement.innerHTML = response.data.name;
@@ -48,7 +49,33 @@ function handleSubmit(event) {
     search(cityInputElement.value);
 }
 
-search("Valencia");
+function displayFahrenheitTemperature(event) {
+    event.preventDefault();
+        let temperatureElement = document.querySelector("#temperature");
+    celciusLink.classList.remove("active");    
+    fahrenheitLink.classList.add("active"); 
+    let fahrenheitTemperature = (celciusTemperature * 9) / 5 + 32; 
+    temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
+}
+
+function displayCelciusTemperature(event) {
+    event.preventDefault();
+    celciusLink.classList.add("active");
+    fahrenheitLink.classList.remove("active");
+    let temperatureElement = document.querySelector("#temperature");
+    temperatureElement.innerHTML = Math.round(celciusTemperature);    
+}
+
+let celciusTemperature = null; 
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
+
+let celciusLink = document.querySelector("#celcius-link");
+celciusLink.addEventListener("click",displayCelciusTemperature);
+
+
+search("Valencia");
